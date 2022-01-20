0f7789ae7a941411fc9f3660b3c27ec6c1283225
martinorion <martinhox@gmail.com>
2022-01-20 08:48:49 +0100

    Spring data Jpa c6(POST)

--- src/main/java/com/appslab/springbootapp/ACompanyController.java
+++ src/main/java/com/appslab/springbootapp/ACompanyController.java
@@ -2,7 +2,8 @@ package com.appslab.springbootapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,24 +14,33 @@ public class ACompanyController {
     @Autowired
     private AddressService addressService;
 
+    @Autowired
+    private ACompanyRepository aCompanyRepository;
+
     @GetMapping("/company")
     public String returnCompany(){
         ACompany aCompany = new ACompany();
         aCompany.setName("Michal");
-         Address address = new Address();
+        Address address = new Address();
         address.setStreet("BBBB");
         address.setZipCode(5);
         address.setCity("CCCC");
         address.setState("DDDD");
 
-       // ACompany aCompany1 = new ACompany();
+        // ACompany aCompany1 = new ACompany();
         // aCompany1.setName("Martin");
-      aCompany.setAddress(address);
+        aCompany.setAddress(address);
         addressService.saveAddress(address);
         aCompanyService.saveACompany(aCompany);
 
 
         //aCompanyService.saveACompany(aCompany1);
         return aCompany.getName();
+    }
+
+    @PostMapping("/company")
+    ACompany newCompany(@RequestBody ACompany newCompany) {
+        addressService.saveAddress(newCompany.getAddress());
+        return aCompanyRepository.save(newCompany);
     }
 }
